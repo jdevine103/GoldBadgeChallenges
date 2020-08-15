0ce19892bd5749fe5bdf565a4aa40eb9c55be369
John Devine <jdevine103@gmail.com>
2020-08-15 10:35:03 -0400

    MenuRepo and Menu skeletons. No methods

---
+++ ChallengeOne_Repo/Menu.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChallengeOne_Repo
+{
+    public class Menu
+    {
+        //constructors
+        public Menu() { }
+        public Menu (int mealNumber, string mealName, string mealDesc, List<string> ingredients, decimal price) 
+        {
+            MealNumber = mealNumber;
+            MealName = mealName;
+            MealDesc = mealDesc;
+            Ingredients = ingredients;
+            Price = price;
+        }
+
+        //properties
+        public int MealNumber { get; set; }
+        public string MealName { get; set; }
+        public string MealDesc { get; set; }
+        public List<string> Ingredients { get; set; }
+        public decimal Price { get; set; }
+    }
+}
--- ChallengeOne_Repo/MenuRepository.cs
+++ ChallengeOne_Repo/MenuRepository.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace ChallengeOne_Repo
 {
-    public class Class1
+    public class MenuRepository
     {
+        private readonly List<Menu> _menu = new List<Menu>();
     }
 }
